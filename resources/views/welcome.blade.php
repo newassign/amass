cc919a413befdfb0d0d54c63a80dc331626492c6
chuqi <qi.chu@bqjr.cn>
2016-07-29 14:53:24 +0800

    更改测试页面。

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -42,7 +42,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Welcome!</div>
+                <div class="title">Welcome !</div>
             </div>
         </div>
     </body>
